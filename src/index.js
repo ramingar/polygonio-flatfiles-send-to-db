4f16d81bebcdc963ce5a176cd42d06cc2870d53a
Rafael Minguet <rminguet@gmail.com>
2024-03-21 21:10:21 +0100

    Reading flatfiles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import {promises as fs} from 'fs'
 import path from 'path';
 import config from './config/config.js'
-import {ConsoleLog, Map, PathJoin} from './utils/utils.js';
+import {ConsoleLog, Map, PathJoin, PromiseAllSettled, ReadFile} from './utils/utils.js';
 
 const flatFilesFolder = config.flatFilesFolder;
 
 fs
     .readdir(flatFilesFolder)
     .then(Map(PathJoin(path, flatFilesFolder)))
-    .then(ConsoleLog)
+    .then(Map(ReadFile(fs)))
+    .then(PromiseAllSettled)
+    .then(val => val)
+    .then(Map(ConsoleLog))
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -31,9 +31,10 @@ const mapStandard = (fnTransform, array) => {
  * @param {args: any} args The argument(s) to be passed to the map function.
  * @returns {(function(*): *)|Array} (function): The curried version of the map function if one argument is provided. (array): The result of directly applying the map function to the given array if two arguments are provided.
  */
-const Map        = (...args) => args.length === 1 ? mapCurried(args[0]) : mapStandard(...args)
-const ConsoleLog = text => console.log(text)
-const ReadFile   = (fs, encoding = 'utf8') => filePath => fs.readFile(filePath, encoding)
-const PathJoin   = (path, folderPath) => filename => path.join(folderPath, filename)
+const Map               = (...args) => args.length === 1 ? mapCurried(args[0]) : mapStandard(...args)
+const PromiseAllSettled = promises => Promise.allSettled(promises)
+const ConsoleLog        = text => console.log(text)
+const ReadFile          = (fs, encoding = 'utf8') => filePath => fs.readFile(filePath, encoding)
+const PathJoin          = (path, folderPath) => filename => path.join(folderPath, filename)
 
-export {Map, ConsoleLog, ReadFile, PathJoin}
+export {Map, ConsoleLog, PromiseAllSettled, ReadFile, PathJoin}
